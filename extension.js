91e4d186757de9777d791283ef6d0f3073b661b8
mouzer <mawzer@gmail.com>
2015-02-05 21:52:06 +0100

    Updated to version 1.1.
    Getting video URL from mp4 path directly.

--- extension.js
+++ extension.js
@@ -3,19 +3,56 @@ function getVideoParent()
 	return document.getElementById('myVideoContainer');
 }
 
-function getVideoObject(videoParent)
+function getSilverlightObject(videoParent)
 {
 	return $(videoParent).find(".pf-silverlight");
 }
 
-function start()
+function getVideoObject(videoParent)
 {
+	return $(videoParent).find(".pf-video");
+}
+
+function getVideoURL()
+{
+	var videoURL = tryMP4Method();
+
+	if (!videoURL || videoURL.length == 0)
+		videoURL = trySilverlightMethod();
+
+	return videoURL;
+}
+
+function tryMP4Method()
+{
+	console.log('Trying MP4 method...');
+
 	var videoParent = getVideoParent();
 
 	if (!videoParent)
 		return;
 
 	var videoObject = getVideoObject(videoParent);
+
+	if (!videoObject)
+		return;
+
+	var sourceObject = videoObject.find('source');
+	var videoURL = sourceObject.attr('src');
+
+	return videoURL;
+}
+
+function trySilverlightMethod()
+{
+	console.log('Trying Silverlight method...');
+
+	var videoParent = getVideoParent();
+
+	if (!videoParent)
+		return;
+
+	var videoObject = getSilverlightObject(videoParent);
 
 	if (!videoObject)
 		return;
@@ -50,6 +87,23 @@ function start()
 	count = end - start;
 
 	var videoURL = 'http://prd.video.mediaset.net/mp4/' + mediaURL.substr(start, count) + '-10_0.mp4';
+	return videoURL;
+}
+
+function start(event)
+{
+	if (event.target.className != 'pf-silverlight' &&
+		event.target.className != 'pf-video')
+		return;
+
+	var videoURL = getVideoURL(videoObject);
+
+	if (!videoURL)
+	{
+		console.log('Video URL not found.');
+		return;
+	}
+
 	window.location.replace(videoURL);
 
 	console.log('Video URL: ' + videoURL);
